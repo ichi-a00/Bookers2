aa07e4b152b3126f8b29270f60ae7ec95ca6e494
ichi-a00 <prenehru@gmail.com>
2021-09-08 13:37:22 +0000

    [FIX]aboutページ

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
+  get  "homes/about"  => "homes#about"
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
