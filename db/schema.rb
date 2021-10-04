758cd5133b9b4c4bc3d96dc6dd9ed1a4927bc697
ichi-a00 <prenehru@gmail.com>
2021-10-04 13:28:24 +0000

    [START]ex8a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_065158) do
+ActiveRecord::Schema.define(version: 2021_10_04_132647) do
 
   create_table "books", force: :cascade do |t|
     t.text "title"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2021_09_18_065158) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "entries", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
@@ -35,9 +42,23 @@ ActiveRecord::Schema.define(version: 2021_09_18_065158) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
